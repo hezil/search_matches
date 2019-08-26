0370176562d7b90ad67ff5d2f8440ab6066f21ce
hezil <31658384+hezil@users.noreply.github.com>
2019-08-26 13:57:26 +0300

    Update setup.py

--- package_example/setup.py
+++ package_example/setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup
 
+setup(
+    name='search_matches',
+    version='0.1',
+    py_modules=['search_matches'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        #'os',
+        #'re',
+        #'shlex',
+        #'subprocess',
+        'pathlib',
+
+    ],
+    entry_points='''
+        [console_scripts]
+        search_matches=search_matches:cli
+    ''',
+)
